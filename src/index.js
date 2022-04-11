c48752e89f5a9e176d201cb1d82c7c8e77461737
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-12 00:24:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const inputSearchref = document.querySelector('#search-box');
+inputSearchref.addEventListener('input', debounce(fetchCountries, 300));
 
 const DEBOUNCE_DELAY = 300;
+
+const URL = 'https://restcountries.com/v2/all';
+
+// console.log();
+
+fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages')
+  .then(response => {
+    return response.json();
+  })
+  .then(countries => {
+    console.log(countries[200].name);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function fetchCountries(name) {}
+
+function markupCountryInfo() {
+  document.querySelector('.country-info').innerHTML = `<div class="country">
+        <img src="https://flagcdn.com/ch.svg" width="50" alt=""/>
+        <span class="country-name">Switzerland</span>
+      </div>
+      <p><span>Capital: </span>Bern</p>
+      <p><span>Population: </span>8341600</p>
+      <p><span>Language: </span>German,French,Italian</p>`;
+}
+
+markupCountryInfo();
+
+function markupCountryList() {
+  document.querySelector('.country-list').innerHTML = `<li class="country-list-item">
+        <img src="https://flagcdn.com/ua.svg" width="50" alt="" class="country-list-img" />
+        <span class="country-list-name">Ukraine</span>
+      </li>`;
+}
+markupCountryList();
