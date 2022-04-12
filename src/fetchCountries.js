97d2b2eb952170808a3c2e7488ae32e213b72e12
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-12 16:19:50 +0300

    Update fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+import Notiflix from 'notiflix';
+
+export default function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`,
+  ).then(response => {
+    if (!response.ok) return Notiflix.Notify.failure('Oops, there is no country with that name');
+    return response.json();
+  });
+}
